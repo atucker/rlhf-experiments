6c865eb8ef2ea5620ceac64e368257a11dae1807
wz-ml <willzhou97@ucla.edu>
2024-10-15 19:14:00 +0000

    Misc changes

--- scripts/tldr/sft.sh
+++ scripts/tldr/sft.sh
@@ -1,1 +1,1 @@
-accelerate launch --config_file accelerate_cfgs/deepspeed_config.yaml --main_process_port 29083 --num_processes 8 src/tldr/sft.py
+accelerate launch --num_processes 8 --mixed-precision bf16 src/tldr/sft.py
