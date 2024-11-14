8d48b8a24106bdfb1eea1f1967696cea590b6f4c
wz-ml <willzhou97@ucla.edu>
2024-11-14 17:51:02 +0000

    Fix gradient norm tracking code

--- src/dips/kl.py
+++ src/dips/kl.py
@@ -39,135 +39,9 @@ import random
 
 # Package imports
 from dips.utils import set_seed, get_grad_norms
+from dips.parse import Args, TaskHParams, PpoHParams, RewardHParams, AdaptiveKLParams, validate_args
 
 wandb.login(key=os.environ["WANDB_API_KEY"])
-
-@dataclass
-class AdaptiveKLParams:
-    target: float = 6.0
-    horizon: int = 10000  # in episodes
-
-@dataclass
-class RewardHParams:
-    use_adaptive_kl: bool = False
-    adaptive_kl: Optional[AdaptiveKLParams] = field(default_factory=AdaptiveKLParams)
-    kl_coef: float = 0.05
-
-@dataclass
-class PpoHParams:
-    num_updates: tyro.conf.Suppress[int] = None
-    noptepochs: int = 1 # number of epochs to train on each PPO update
-    vf_coef: float = 0.1
-    cliprange: float = 0.2
-    cliprange_value: float = 0.2
-    gamma: float = 1
-    lam: float = 0.95
-    whiten_rewards: bool = True
-
-
-@dataclass
-class TaskHParams:
-    # Query params
-    query_length: int = 512
-    query_dataset: str = "cleanrl/summarize_from_feedback_tldr_3_filtered_oai_preprocessing_1704563162" # pythia 2.9
-
-    # Response params
-    response_length: int = 53
-
-    # Truncate response after the first occurrence of this token at or after index after when sampling.
-    truncate_token: Literal["eos"] = "eos"
-    truncate_token_id: Optional[int] = None
-    penalty_reward_value: int = -1
-
-    # LM params
-    temperature: float = 0.7
-
-
-@dataclass
-class Args:
-    train_dips: bool = False # whether to train via DIPS or RLOO
-    disable_wandb: bool = False
-    # common args
-    exp_name: str = "pythia"
-    """the name of this experiment"""
-    seed: int = 55134
-    """seed of the experiment"""
-    track: bool = True
-    """if toggled, this experiment will be tracked with Weights and Biases"""
-    wandb_project_name: str = "tldr_summarize_pythia_will"
-    """the wandb's project name"""
-    cuda: bool = True
-    """Whether to use cuda if available."""
-    run_name: Optional[str] = None
-    """a unique name of this run"""
-    push_to_hub: bool = False
-    "whether to upload the saved model to huggingface"
-    hf_entity: str = ""
-    "the user or org name of the model repository from the Hugging Face Hub"
-    deepspeed: bool = False
-    """Whether to use deepspeed to train the model"""
-    print_sample_output_freq: int = 1000
-    """How often to print sample output"""
-    run_eval: bool = True
-    """Whether to run evaluation"""
-
-    # optimizer args
-    eps: float = 1e-5
-    """the epsilon value for the optimizer - an extremely small value to prevent division by zero"""
-    lr: float = 3e-6
-    """the learning rate"""
-    optimizer: Literal["adam", "adamw"] = "adamw"
-    """Which optimizer to use"""
-    scheduler: str = "cosine"
-    """Which scheduler to use"""
-    warm_up_steps: int = 0
-    """Number of warm up steps for the scheduler"""
-
-    gradient_accumulation_steps: int = 8
-    """The number of gradient accumulation steps"""
-
-    # ------ Batch Size in Memory / GPU: per_device_train_batch_size --------
-    rloo_k: int = 4 # number of samples to use for RLOO
-    
-    per_device_train_batch_size: int = 2
-    """The micro batch size per GPU (HF's `per_device_train_batch_size`)"""
-    per_device_eval_batch_size: int = 2
-    """per rank eval batch size"""
-    local_rollout_forward_batch_size: int = 2
-    """per rank no grad forward pass in the rollout phase"""
-
-    total_episodes: int = int(1e6) # Informs the number of ppo updates to do
-    """The total number of episodes in the dataset"""
-
-    # optional args filled while running
-    world_size: Optional[int] = 2
-    """The number of processes (GPUs) to use"""
-
-    # other args
-    base_model: str = "models/sft_tldr_pythia_1_4b"
-    """the name of the pretrained model to use"""
-    offload: bool = False
-    """Whether to offload ref policy and reward model to CPU"""
-    reward_model_path: str = "models/rm_sft_tldr_pythia_1_4b"
-    """the name of the pretrained model to use"""
-    sft_model_path: str = "models/sft_tldr_pythia_1_4b"
-    """the name of the pretrained model to use"""
-    dropout_layer_keys: List[str] = field(
-        default_factory=lambda: ["attn_pdrop", "embd_pdrop", "resid_pdrop", "summary_first_dropout"]
-    )
-    """Which layers to apply dropout to"""
-    output_dir: str = "models/tldr_pythia_1_4b"
-    """Where to save the model"""
-    lora_rank: int = 1024
-    """the rank of the lora matrix"""
-    lora_alpha: int = 2048
-    """weight of lora"""
-    lora_dropout: float = 0.0
-    """dropout for lora"""
-    task: TaskHParams = field(default_factory=TaskHParams)
-    reward: RewardHParams = field(default_factory=RewardHParams)
-    ppo: PpoHParams = field(default_factory=PpoHParams)
-
 
 # taken from https://github.com/microsoft/DeepSpeedExamples/blob/737c6740bec38b77a24a59135b6481a53d566b38/applications/DeepSpeed-Chat/training/utils/model/model_utils.py#L20C1-L26C52
 def configure_dropout(model_config, dropout_layer_keys, dropout):
@@ -511,14 +385,11 @@ if __name__ == "__main__":
     local_seed = args.seed + accelerator.process_index * 100003  # Prime
     set_seed(local_seed)
 
+    validate_args(args)
+
     args.world_size = accelerator.num_processes
     args.batch_size = args.per_device_train_batch_size * args.world_size * args.gradient_accumulation_steps
     args.local_batch_size = args.per_device_train_batch_size * args.gradient_accumulation_steps
-    if args.ppo.whiten_rewards:
-        assert (
-            args.local_batch_size >= 8
-        ), f"Per-rank minibatch size {args.local_batch_size} is insufficient for whitening"
-        # raise NotImplementedError("Whitening is not supported at the moment.")
     args.ppo.num_updates = args.total_episodes // args.batch_size
 
     tokenizer = AutoTokenizer.from_pretrained(
@@ -866,10 +737,13 @@ if __name__ == "__main__":
                         approx_kl = new_logprobs - mb_ref_logprobs
                         prob_ratio = torch.exp(new_logprobs - mb_logprobs)
                         weighting = (mb_reward - mb_baseline - kl_ctl.value * approx_kl)
-                        # loss = torch.mean(-1 * prob_ratio * weighting)
-                        policy_loss_term = -1 * (prob_ratio * weighting.detach()).mean()
-                        kl_loss_term = -1 * (prob_ratio.detach() * weighting).mean()
-                        loss = policy_loss_term + kl_loss_term
+
+                        if args.factor_loss:
+                            policy_loss_term = -1 * (prob_ratio * weighting.detach()).mean()
+                            kl_loss_term = -1 * (prob_ratio.detach() * weighting).mean()
+                            loss = policy_loss_term + kl_loss_term
+                        else:
+                            loss = torch.mean(-1 * prob_ratio * weighting)
 
                     else:
                         # RLOO loss
@@ -880,6 +754,19 @@ if __name__ == "__main__":
 
                     # Grab model grad norms
                     if args.train_dips:
+                        with torch.no_grad():
+                            policy_term_grad_norms = torch.autograd.grad(
+                                outputs = policy_loss_term,
+                                inputs = param_subset,
+                                create_graph = False,
+                                retain_graph = True,
+                            )
+                            kl_term_grad_norms = torch.autograd.grad(
+                                outputs = policy_loss_term,
+                                inputs = param_subset,
+                                create_graph = False,
+                                retain_graph = True,
+                            )
                         accelerator.backward(policy_loss_term, retain_graph = True)
                         policy_term_grad_norms = get_grad_norms(param_subset, device = device)
                         optimizer.zero_grad()
---
+++ src/dips/parse.py
@@ -0,0 +1,141 @@
+from dataclasses import asdict, dataclass, field
+from typing import List, Literal, Optional, Tuple, Union
+import tyro
+
+@dataclass
+class AdaptiveKLParams:
+    target: float = 6.0
+    horizon: int = 10000  # in episodes
+
+@dataclass
+class RewardHParams:
+    use_adaptive_kl: bool = False
+    adaptive_kl: Optional[AdaptiveKLParams] = field(default_factory=AdaptiveKLParams)
+    kl_coef: float = 0.05
+
+@dataclass
+class PpoHParams:
+    num_updates: tyro.conf.Suppress[int] = None
+    noptepochs: int = 1 # number of epochs to train on each PPO update
+    vf_coef: float = 0.1
+    cliprange: float = 0.2
+    cliprange_value: float = 0.2
+    gamma: float = 1
+    lam: float = 0.95
+    whiten_rewards: bool = True
+
+
+@dataclass
+class TaskHParams:
+    # Query params
+    query_length: int = 512
+    query_dataset: str = "cleanrl/summarize_from_feedback_tldr_3_filtered_oai_preprocessing_1704563162" # pythia 2.9
+
+    # Response params
+    response_length: int = 53
+
+    # Truncate response after the first occurrence of this token at or after index after when sampling.
+    truncate_token: Literal["eos"] = "eos"
+    truncate_token_id: Optional[int] = None
+    penalty_reward_value: int = -1
+
+    # LM params
+    temperature: float = 0.7
+
+
+@dataclass
+class Args:
+    train_dips: bool = False # whether to train via DIPS or RLOO
+    disable_wandb: bool = False
+    factor_loss: bool = False
+    # common args
+    exp_name: str = "pythia"
+    """the name of this experiment"""
+    seed: int = 55134
+    """seed of the experiment"""
+    track: bool = True
+    """if toggled, this experiment will be tracked with Weights and Biases"""
+    wandb_project_name: str = "tldr_summarize_pythia_will"
+    """the wandb's project name"""
+    cuda: bool = True
+    """Whether to use cuda if available."""
+    run_name: Optional[str] = None
+    """a unique name of this run"""
+    push_to_hub: bool = False
+    "whether to upload the saved model to huggingface"
+    hf_entity: str = ""
+    "the user or org name of the model repository from the Hugging Face Hub"
+    deepspeed: bool = False
+    """Whether to use deepspeed to train the model"""
+    print_sample_output_freq: int = 1000
+    """How often to print sample output"""
+    run_eval: bool = True
+    """Whether to run evaluation"""
+
+    # optimizer args
+    eps: float = 1e-5
+    """the epsilon value for the optimizer - an extremely small value to prevent division by zero"""
+    lr: float = 3e-6
+    """the learning rate"""
+    optimizer: Literal["adam", "adamw"] = "adamw"
+    """Which optimizer to use"""
+    scheduler: str = "cosine"
+    """Which scheduler to use"""
+    warm_up_steps: int = 0
+    """Number of warm up steps for the scheduler"""
+
+    gradient_accumulation_steps: int = 8
+    """The number of gradient accumulation steps"""
+
+    # ------ Batch Size in Memory / GPU: per_device_train_batch_size --------
+    rloo_k: int = 4 # number of samples to use for RLOO
+    
+    per_device_train_batch_size: int = 2
+    """The micro batch size per GPU (HF's `per_device_train_batch_size`)"""
+    per_device_eval_batch_size: int = 2
+    """per rank eval batch size"""
+    local_rollout_forward_batch_size: int = 2
+    """per rank no grad forward pass in the rollout phase"""
+
+    total_episodes: int = int(1e4) # Informs the number of ppo updates to do
+    """The total number of episodes in the dataset"""
+
+    # optional args filled while running
+    world_size: Optional[int] = 2
+    """The number of processes (GPUs) to use"""
+
+    # other args
+    base_model: str = "models/sft_tldr_pythia_1_4b"
+    """the name of the pretrained model to use"""
+    offload: bool = False
+    """Whether to offload ref policy and reward model to CPU"""
+    reward_model_path: str = "models/rm_sft_tldr_pythia_1_4b"
+    """the name of the pretrained model to use"""
+    sft_model_path: str = "models/sft_tldr_pythia_1_4b"
+    """the name of the pretrained model to use"""
+    dropout_layer_keys: List[str] = field(
+        default_factory=lambda: ["attn_pdrop", "embd_pdrop", "resid_pdrop", "summary_first_dropout"]
+    )
+    """Which layers to apply dropout to"""
+    output_dir: str = "models/tldr_pythia_1_4b"
+    """Where to save the model"""
+    lora_rank: int = 1024
+    """the rank of the lora matrix"""
+    lora_alpha: int = 2048
+    """weight of lora"""
+    lora_dropout: float = 0.0
+    """dropout for lora"""
+    task: TaskHParams = field(default_factory=TaskHParams)
+    reward: RewardHParams = field(default_factory=RewardHParams)
+    ppo: PpoHParams = field(default_factory=PpoHParams)
+
+def validate_args(args: Args):
+    if args.factor_loss:
+        assert args.train_dips, "Loss factoring only supports DIPS. Are you using --train_dips?"
+    assert args.rloo_k >= 1
+
+    if args.ppo.whiten_rewards:
+        assert (
+            args.local_batch_size >= 8
+        ), f"Per-rank minibatch size {args.local_batch_size} is insufficient for whitening"
+        # raise NotImplementedError("Whitening is not supported at the moment.")
